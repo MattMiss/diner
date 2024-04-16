3214c92b8430c45b4dab4f153ed907ee571f8532
Matt Miss <matt.w.miss@gmail.com>
2024-04-16 13:50:33 -0400

    Added Order Routes

--- index.php
+++ index.php
@@ -41,5 +41,36 @@ $f3->route('GET /menus/dinner', function() {
     echo $view->render('views/dinner-menu.html');
 });
 
+// Define an Order 1 route
+$f3->route('GET|POST /order1', function() {
+    global $f3;
+    // If the form has been posted
+    if ($_SERVER['REQUEST_METHOD'] == "POST"){
+        echo "<p>You got here using the POST method</p>";
+        var_dump($_POST);
+
+        // Get the data from the POST array
+        $food = $_POST['food'];
+        $meal = $_POST['meal'];
+
+        if (true){
+            $f3->set('SESSION.food', $food);
+            $f3->set('SESSION.meal', $meal);
+        }
+    }else {
+        echo "<p>You got here using the GET method</p>";
+    }
+    // Render a view page
+    $view = new Template();
+    echo $view->render('views/order1.html');
+});
+
+// Define an Order 1 route
+$f3->route('GET /order2', function() {
+    // Render a view page
+    $view = new Template();
+    echo $view->render('views/order2.html');
+});
+
 // Run fat free
 $f3->run();
